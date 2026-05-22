94b3b2a125f1d7322d03d3aa6ed3d22cfa26d581
magda chikhradz <magda292005@gmail.com>
2026-05-22 14:38:43 +0400

    work on it

--- TASK_5/main.js
+++ TASK_5/main.js
@@ -1,2 +1,11 @@
 //5.1
 
+const firstDiv = document.querySelector(".container");
+const firstInput = document.getElementById("username");
+const allSpans = document.querySelectorAll(".message");
+
+console.log(firstDiv);
+console.log(firstInput);
+console.log(allSpans);
+
+//5.2
