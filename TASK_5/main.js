5bd0eaef503d755ef7a4914e5895d432b5624127
magda chikhradz <magda292005@gmail.com>
2026-05-22 14:59:29 +0400

    task_5 done

--- TASK_5/main.js
+++ TASK_5/main.js
@@ -9,3 +9,28 @@ console.log(firstInput);
 console.log(allSpans);
 
 //5.2
+
+firstDiv.style.backgroundColor = "red";
+firstDiv.style.marginBottom = "30px";
+
+allSpans.forEach((item) => {
+  item.style.fontSize = "30px";
+});
+
+//5.3
+
+const Newbtn = document.createElement("button");
+
+Newbtn.setAttribute("class", "sendBtn");
+
+Newbtn.innerHTML = `Send`;
+
+console.log(Newbtn);
+
+firstDiv.append(Newbtn);
+
+//5.4
+
+firstDiv.addEventListener("click", () => {
+  console.log("it’s clicked");
+});
